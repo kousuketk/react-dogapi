e42655f45e058778436d9270d9d6c08719bc71c2
kousuke takahashi <pamarikomi_t@icloud.com>
2021-12-29 14:19:11 +0900

    null判定

--- src/App.js
+++ src/App.js
@@ -22,8 +22,15 @@ function Image(props) {
   );
 }
 
+function Loading() {
+  return <p>Loading...</p>;
+}
+
 function Gallery(props) {
   const { urls } = props;
+  if (urls == null) {
+    return <Loading />;
+  }
   return (
     <div className="columns is-vcentered is-multiline">
       {urls.map((url) => {
